e94da59e0c04f68c142ef5d245040bfb353dea23
MisterLobo <algae1234567890@gmail.com>
2025-05-22 06:42:30 +0800

    updated utils

--- apps/ebs-app/src/lib/utils.ts
+++ apps/ebs-app/src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+import { isFuture, toDate } from "date-fns";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,4 +12,12 @@ export async function importURLPatternPolyfill() {
   if (!globalThis.URLPattern) { 
     await import("urlpattern-polyfill");
   }
+}
+
+export function isUpcoming(initialDate: string) {
+  const d = toDate(initialDate)
+  const nd = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes())
+  
+  const result = isFuture(nd)
+  return result
 }

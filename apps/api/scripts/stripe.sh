8f468963c170ec1037aa691398e84431401bea47
MisterLobo <algae1234567890@gmail.com>
2025-05-23 17:02:45 +0800

    update stripe script

--- apps/api/scripts/stripe.sh
+++ apps/api/scripts/stripe.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/bin/stripe listen --forward-to http://127.0.0.1:9090/webhook/stripe --skip-verify
+/bin/stripe listen --forward-to http://127.0.0.1:9090/api/v1/webhook/stripe --skip-verify

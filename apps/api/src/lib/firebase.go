614d58c2445de717548a04e2e0ad47af93431af0
MisterLobo <algae1234567890@gmail.com>
2025-06-06 14:57:20 +0800

    api: add firebase init func

--- apps/api/src/lib/firebase.go
+++ apps/api/src/lib/firebase.go
@@ -11,22 +11,40 @@ import (
 	"google.golang.org/api/option"
 )
 
+var innerApp *firebase.App
+var innerAuth *auth.Client
+
 func GetFirebaseAuth() (*auth.Client, error) {
 	cwd, _ := os.Getwd()
 	log.Println("cwd:", cwd)
 	secretsPath := os.Getenv("SECRETS_DIR")
 	opt := option.WithCredentialsFile(path.Join(secretsPath, "admin-sdk-credentials.json"))
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		log.Fatalf("error initializing app: %v\n", err.Error())
-		return nil, err
+	if innerApp == nil {
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			log.Fatalf("error initializing app: %v\n", err.Error())
+			return nil, err
+		}
+		innerApp = app
 	}
 
+	if innerAuth == nil {
+		auth, err := innerApp.Auth(context.Background())
+		if err != nil {
+			log.Fatalf("error initializing Firebase Auth: %v\n", err.Error())
+			return nil, err
+		}
+		innerAuth = auth
+	}
+
+	return innerAuth, nil
+}
+
+func NewFirebaseApp(app *firebase.App) {
+	innerApp = app
 	auth, err := app.Auth(context.Background())
 	if err != nil {
-		log.Fatalf("error initializing auth: %v\n", err.Error())
-		return nil, err
+		log.Fatalf("error initializing Firebase Auth: %s\n", err.Error())
 	}
-
-	return auth, nil
+	innerAuth = auth
 }

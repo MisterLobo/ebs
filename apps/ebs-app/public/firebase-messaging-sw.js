93ed5138d50ec86d84d741d290b3d387a026bd50
MisterLobo <algae1234567890@gmail.com>
2025-06-17 23:02:12 +0800

    app: use placeholder for sw config

--- apps/ebs-app/public/firebase-messaging-sw.js
+++ apps/ebs-app/public/firebase-messaging-sw.js
@@ -2,13 +2,10 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js")
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js")
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDxiqPn092xxYRF0B-pOjntMD4uzg77AZs',
-  // authDomain: 'events-booking-system-8184a.firebaseapp.com',
-  projectId: 'events-booking-system-8184a',
-  // storageBucket: 'events-booking-system-8184a.firebasestorage.app',
-  messagingSenderId: '948261007536',
-  appId: '1:948261007536:web:241dc42b2749739f2ecb75',
-  // measurementId: 'your_keys',
+  apiKey: 'API_KEY',
+  projectId: 'PROJECT_ID',
+  messagingSenderId: 'SENDER_ID',
+  appId: 'APP_ID',
 };
 // eslint-disable-next-line no-undef
 firebase.initializeApp(firebaseConfig)
